8c07b5ffbd84cefb4db27b12ee8db55510d8ef15
Jagruti Hota <jagrutihota92@gmail.com>
2024-12-22 13:51:29 +0530

    Login Integration Done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,72 @@
+import axios from "axios";
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      <h1>This is the login Page</h1>
-    </div>
-  )
-}
+  const [emailId,setEmailId] =useState("")
+  const [password, setPassword] = useState("");  
 
-export default Login
+   async function handleSubmit(){
+
+    try {
+      const data= await axios.post("http://localhost:3000/login", {
+        emailId,
+        password,
+      }, {withCredentials: true});
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+     
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="card bg-base-100 image-full w-[30vw] h-[50vh]  shadow-xl">
+        <figure>
+          <img src="/login-bg.jpg" alt="loginBg" />
+        </figure>
+        <div className="card-body">
+          <h2 className="text-center text-xl font-bold">Login</h2>
+          <div className="flex flex-col items-center justify-center w-full h-full">
+            <label className="input input-bordered flex items-center gap-2 m-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+              <input type="email" className="grow" placeholder="Enter Your Email Address" value={emailId} onChange={(e) => setEmailId(e.target.value)}/>   
+            </label>
+
+            <label className="input input-bordered flex items-center gap-2 m-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input type="password" className="grow" value={password}  placeholder="Enter the password" onChange={(e) => setPassword(e.target.value)} />
+            </label>
+          </div>
+
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary hover:btn-primary px-6" onClick={handleSubmit}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
